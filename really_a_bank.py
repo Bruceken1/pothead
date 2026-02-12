d46526ae8fbaabb9a3fb592166586249dd6d7e6b
Bruceken1 <mukabiken@gmail.com>
2026-02-12 12:39:09 +0300

    Second commit

--- really_a_bank.py
+++ really_a_bank.py
@@ -4,15 +4,17 @@ initial_balance = float(input("What is your current balance? : "))
 avg_deposit = float(input("What is your current salary: "))
 avg_withdrawals = float(input("How much will you be spending monthly: "))
 total_months = int(input("How many months do you want the sim to run: "))
-interest_rate = float(input("What interest rate do you want to apply annualy: "))
+interest_rate = float(input("What interest rate (in decimals) do you want to apply annualy: "))
 var_factor = float(input("What var factor do you want to use (decimals): "))
 safety_threshold = float(input("What is your safety threshold: "))
 reduction_percent = float(input("What percentage of reduction do you expect after balance is below threshold? : "))
 
-def sim(initial_balance, avg_deposit, avg_withdrawals, total_months,interest_rate):
+def sim(initial_balance, avg_deposit, avg_withdrawals, total_months,interest_rate, var_factor, safety_threshold, reduction_percent):
     
-    if initial_balance < 0 or avg_deposit < 0 or avg_withdrawals < 0 or interest_rate < 0 :
-        return None, 0 , False, []
+    if any(x < 0  for x in [initial_balance, avg_deposit, avg_withdrawals, total_months, interest_rate, var_factor,safety_threshold, reduction_percent]):
+    
+    
+        return None, 0 , False, [], total_months
     
     balance = initial_balance
     months = 0
@@ -23,33 +25,37 @@ def sim(initial_balance, avg_deposit, avg_withdrawals, total_months,interest_rat
         
         balance += balance*(interest_rate/12)
         
-        balance += avg_deposit * random.uniform(1-var_factor, 1 + var_factor)
+        deposit = avg_deposit * random.uniform(1-var_factor, 1 + var_factor)
         
-        balance +- avg_withdrawals * random.uniform(1 - var_factor, 1 + var_factor)
+        withdrawals = avg_withdrawals * random.uniform(1 - var_factor, 1 + var_factor)
         
-        monthly_balances.append(balance)
         
         if balance < safety_threshold:
-            new_avg_withdrawals = avg_withdrawals - (avg_withdrawals * reduction_percent)
-            
-            balance +- new_avg_withdrawals
+            withdrawals *= (1 - reduction_percent)
+
+        balance += deposit - withdrawals
+        
+        monthly_balances.append(balance)                    
             
         if balance < 0 :
             bankrupt = True
+            months += 1
             break
         
         months += 1
         
     return balance, months, bankrupt, monthly_balances, total_months
 
-balance, months, bankrupt, monthly_balances, total_months = sim(initial_balance, avg_deposit, avg_withdrawals, total_months, interest_rate)
+balance, months, bankrupt, monthly_balances, total_months = sim(initial_balance, avg_deposit, avg_withdrawals, total_months, interest_rate, var_factor, safety_threshold, reduction_percent)
 
 if balance is None:
     print("Invalid input. Check your figures.")
 
 else:
+    print("\nSimulation Results")
+    print("-------------------")
     print("Your remaining balance is ", balance)
-    print("The number of months simulated is ", total_months)
-    print("You are bankrupt " if bankrupt else "You are not bankrupt")
-    print("Below is your monthly statement: ", [round(b, 2) for b in monthly_balances])
+    print("\nThe number of months simulated is ", total_months)
+    print("\nYou are bankrupt " if bankrupt else "You are not bankrupt")
+    print("\nBelow is your monthly statement: ", [round(balances, 2) for balances in monthly_balances])
           
